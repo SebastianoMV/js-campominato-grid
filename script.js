8279608b9053d7cbfdcae2d0235d6e8e83f347d2
SebastianoMV <sebastianovisco@gmail.com>
2022-04-21 17:27:39 +0200

    reset difficoltà

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const container = document.querySelector('.smv-container');
-const listNumbers = [];
+
 
 document.querySelector('#start').addEventListener('click',init);
 
 function init(){
+  
+  const container = document.querySelector('.smv-container');
   const difficolta = document.getElementById('difficolta').value;
+  container.innerHTML = '';
   for(let i = 0; i < difficolta; i++){
      const sq = createSquare(container,difficolta)
      sq.innerHTML = `<span>${i}</span>`;
