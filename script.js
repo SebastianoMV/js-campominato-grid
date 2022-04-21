dec92d59142330b64dd0d7eefd7107440796e7e0
SebastianoMV <sebastianovisco@gmail.com>
2022-04-21 16:36:31 +0200

    Avevo capito andasse con numeri random eccolo senza

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function init(){
   const difficolta = document.getElementById('difficolta').value;
   for(let i = 0; i < difficolta; i++){
      const sq = createSquare(container,difficolta)
+     sq.innerHTML = `<span>${i}</span>`;
      sq.addEventListener('click', function(){
         this.classList.add('clicked');
      })
@@ -16,33 +17,6 @@ function init(){
 function createSquare( target , value ){
     const sq = document.createElement( 'div' );
     sq.className = 'square square'+ value;
-    const number = getUniqueRandomNumber( 1, value );
-    sq.innerHTML = `<span>${number}</span>`;
-    sq.classList.add(getOddEven(number));
     target.append(sq);
     return sq;
-}
-
-function getUniqueRandomNumber(min, max){
-    let number = null;
-    let valid = false;
-
-    while(!valid){
-        number = getRandomNumber(min, max);
-        if(!listNumbers.includes(number)){
-            valid = true;
-            listNumbers.push(number)
-        }
-    }
-
-    return number;
-}
-
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function getOddEven(n){
-    if(n % 2) return 'odd';
-    return 'even';
 }
